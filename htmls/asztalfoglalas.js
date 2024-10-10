6d4b847419de9f5e648ef596dabf842e2715ef92
10c-lakatos <10c-lakatos@ipari.vein.hu>
2024-10-10 19:34:01 +0200

    v1.6.0 - Asztalfoglalás a felhasználó oldaláról (működőképes)

--- htmls/asztalfoglalas.js
+++ htmls/asztalfoglalas.js
@@ -1,9 +1,35 @@
 let asztalokszama = 25 // asztalok száma
 let sorokszama = 0
+// 0 - nem foglalt; 1 - foglalt
+var asztalok = {
+
+}
 function asztalfoglalas() {
-    
+    var indexValue = document.getElementById('asztalnumber').value
+    var NameValue = document.getElementById('asztalname').value
+    if (indexValue != "" && indexValue != 0) {
+        if (indexValue <= asztalokszama) {
+            if (NameValue != "") {
+                if (asztalok[indexValue+".asztal"] == "0") {
+                    AsztalValue(indexValue, "1")
+                    alert("Asztal lefoglalva, "+NameValue+" néven!")
+                    location.reload()
+                } else {
+                    alert("Ez az asztal már le van foglalva!")
+                }
+            } else {
+                alert("Nincsen megadva név!")
+            }
+        } else {
+            alert("Túl nagy számot adtál meg!")
+        }
+    } else {
+        alert("Nincsen megfelelő asztalszám!")
+    }
 }
 document.addEventListener('DOMContentLoaded', function() {
+    loadAsztals()
+    console.log(asztalok)
     var sorok = {
         
     }
@@ -13,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
         sorokszama = asztalokszama/6+1
         sorokszama = Math.trunc(sorokszama)
     }
-    if (asztalokszama%5 ==0) {
+    if (asztalokszama%6 ==0) {
         for (let i=1; i <= sorokszama; i++) {
             sorok[String(i)+".sor"] = 6
         }
@@ -26,10 +52,9 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     }
-    console.log(sorok)
-    let asztalok = 1
+
+    let asztalokdb = 1
     for (let i=1; i <= sorokszama; i++) {
-        
         var table = document.getElementById('asztaloktablabody')
         var ujsor = document.createElement("tr")
         ujsor.id = String(i)+".sor"
@@ -37,13 +62,30 @@ document.addEventListener('DOMContentLoaded', function() {
         table.appendChild(ujsor)
         for (let h=1; h <= sorok[String(i)+".sor"]; h++) {
             var ujcella = document.createElement("td")
-            ujcella.id = String(asztalok)+".asztal"
-            ujcella.innerHTML = String(asztalok) + ". asztal"
+            ujcella.id = String(asztalokdb)+".asztal"
+            ujcella.innerHTML = String(asztalokdb) + ". asztal"
+            if (asztalok[String(asztalokdb)+".asztal"] == "1") {
+                ujcella.style.backgroundColor = "crimson"
+            } else {
+                ujcella.style.backgroundColor = "green"
+            }
             document.getElementById(String(i)+".sor").appendChild(ujcella)
-            asztalok += 1
+            asztalokdb += 1
         }   
     }
 })
+// database functionok
+function AsztalValue(index, value) {
+    localStorage.setItem(String(index)+'.asztal', value);
+}
+function loadAsztals() {
+    for (let i=1; i <= asztalokszama; i++) {
+        console.log(i)
+        asztalok[String(i)+".asztal"] = localStorage.getItem(String(i)+'.asztal') || "0";
+    }
+}
+
+// key eventek
 function onlyNumberKey(evt) {
     let ASCIICode = (evt.which) ? evt.which : evt.keyCode
     if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
