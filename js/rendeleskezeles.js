e80e08ba07083fe01030f2a87b0ab6d3b467df80
10c-lakatos <10c-lakatos@ipari.vein.hu>
2024-10-17 10:55:01 +0200

    v1.7.4

--- js/rendeleskezeles.js
+++ js/rendeleskezeles.js
@@ -28,7 +28,42 @@ function loadRendelesek() {
     var ujbutton = document.createElement('button')
     ujbutton.type = "button"
     ujbutton.id = String(index)+".rendeles"
-    ujbutton.innerHTML = "Név: "+item.name
+    ujbutton.onclick = function() { loadListRendeles(index) };
+    ujbutton.innerHTML = "Index: "+index+" | Név: "+item.name+" | E-mail: "+item.email+" | Telefonszám: "+item.telefon+" | Lakcím: "+item.cim+" | Fizetési mód: "+item.paymentmethod+""
     document.getElementById('rendelesek').appendChild(ujbutton)
   });
+}
+function loadListRendeles(index) {
+    let osszeg = 0
+    let rendelesek = JSON.parse(localStorage.getItem('rendelesek')) || [];
+    let kajalista = rendelesek[index].lista
+    const tableBody = document.getElementById('tablazatbody'); 
+  tableBody.innerHTML = '';
+
+  kajalista.forEach((item, index) => {
+    var tablerow = document.createElement('tr');
+    var t1 = document.createElement("th");
+    t1.innerHTML = item.name;
+    var t2 = document.createElement("td");
+    t2.innerHTML = item.ertek + " Ft";
+    osszeg += item.ertek * item.mennyiseg;
+    var t3 = document.createElement("td");
+    t3.innerHTML = item.mennyiseg;
+    tableBody.appendChild(tablerow);
+    tablerow.appendChild(t1);
+    tablerow.appendChild(t2);
+    tablerow.appendChild(t3);
+    });
+    document.getElementById('osszeg').innerText = osszeg;
+  if (kajalista == null || kajalista == "") {
+    var tablerow = document.createElement('tr');
+    var ts1 = document.createElement("td");
+    ts1.innerHTML = "Nincs kiválasztva rendelés!";
+    ts1.colSpan = "4";
+    tableBody.appendChild(tablerow);
+    tablerow.appendChild(ts1);
+  }
+}
+function vissza() {
+    window.location.replace('../index.html')
 }
